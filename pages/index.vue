c17f367a0d66fc15f62bd67993c6aeb8abd01d7e
Memories <935915488@qq.com>
2021-03-16 00:27:24 +0800

    fix: 首次提交

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+      首页
+      <nuxt-link to="./user">用户</nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        123
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style scoped lang="less">
+.container{
+    color: yellowgreen;
+}
+</style>
